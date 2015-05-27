f7d16c6ef6e656b7d5a575ce7e6db0ce8d636fc5
TomFrost <tom@frosteddesign.com>
2015-05-27 16:59:36 -0400

    findPath: Build logging structure for clone question

--- test/vers.js
+++ test/vers.js
@@ -11,18 +11,25 @@ var Vers = require('../lib/vers');
 chai.use(chaiAsPromised);
 
 var vers = new Vers();
-vers.translate(1, 2, '1to2', '2to1');
 vers.translate(1, 3, '1to3', '3to1');
+vers.translate(2, 5, '2to5', '5to2');
 vers.translate(2, 3, '2to3', '3to2');
-vers.translate(2, 4, '2to4', '4to2');
-vers.translate(3, 5, '3to5', '5to3');
+vers.translate(3, 4, '3to4', '4to3');
 vers.translate(4, 5, '4to5', '5to4');
-vers.translate(4, 6, '4to6', '6to4');
 vers.translate(5, 6, '5to6', '6to5');
+
+/*
+ 1  2  3  4  5
+ |_____|
+    |__|
+    |________|
+       |__|
+          |__|
+ */
 
 describe('Vers', function() {
   it('should find the shortest path', function() {
-    var path = vers._findPath(4, 3);
+    var path = vers._findPath(1, 6);
     console.log(path);
   });
 });
