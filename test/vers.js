a6d9b1fc7017443511cd91bb29e78e3deb6ad96d
TomFrost <tom@frosteddesign.com>
2015-05-28 16:17:18 -0400

    Test: Add constructor tests

--- test/vers.js
+++ test/vers.js
@@ -10,19 +10,42 @@ var Vers = require('../lib/vers');
 
 chai.use(chaiAsPromised);
 
-var vers = new Vers();
-vers.translate(1, 2, '1to2', '2to1');
-vers.translate(1, 3, '1to3', '3to1');
-vers.translate(2, 3, '2to3', '3to2');
-vers.translate(2, 4, '2to4', '4to2');
-vers.translate(3, 5, '3to5', '5to3');
-vers.translate(4, 5, '4to5', '5to4');
-vers.translate(4, 6, '4to6', '6to4');
-vers.translate(5, 6, '5to6', '6to5');
-
 describe('Vers', function() {
-  it('should find the shortest path', function() {
-    var path = vers._findPath(4, 3);
-    console.log(path);
+  describe('Constructor', function() {
+    it('should construct with default versionGetter', function() {
+      var inst = new Vers();
+      inst.translate(2, 3, function(obj) {
+        obj.version = 3;
+      });
+      return inst.toLatest({version: 2}).then(function(obj) {
+        should.exist(obj);
+        obj.should.have.property('version').equal(3);
+      });
+    });
+    it('should assume version 1 with default versionGetter', function() {
+      var inst = new Vers();
+      inst.translate(1, 2, function(obj) {
+        obj.version = 2;
+      });
+      return inst.toLatest({}).then(function(obj) {
+        should.exist(obj);
+        obj.should.have.property('version').equal(2);
+      });
+    });
+    it('should accept a custom versionGetter', function() {
+      var inst = new Vers(function(obj) { return obj.v; });
+      inst.translate(1, 2, function(obj) {
+        obj.v = 2;
+      });
+      return inst.toLatest({v: 1}).then(function(obj) {
+        should.exist(obj);
+        obj.should.have.property('v').equal(2);
+      });
+    });
+    it('should construct as a function call', function() {
+      var inst = Vers();
+      should.exist(inst);
+      inst.should.be.instanceOf(Vers);
+    });
   });
 });
