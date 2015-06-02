6e4c1e6981f877285b66232ea7e09fc6d35baf2e
TomFrost <tom@frosteddesign.com>
2015-06-02 00:24:47 -0400

    Test: Add coverage

--- test/Vers.js
+++ test/Vers.js
@@ -23,14 +23,21 @@ describe('Vers', function() {
       return inst.toLatest({}).should.become({version: 2});
     });
     it('should accept a custom versionGetter', function() {
-      var inst = new Vers(function(obj) { return obj.v; });
-      inst.translate(1, 2, function(obj) { obj.v = 2; });
-      return inst.toLatest({v: 1}).should.become({v: 2});
+      var inst = new Vers({getVersion: function(obj) { return obj.v; }});
+      inst.translate(2, 3, function(obj) { obj.v = 3; });
+      return inst.toLatest({v: 2}).should.become({v: 3});
     });
     it('should construct as a function call', function() {
       var inst = Vers();
       should.exist(inst);
       inst.should.be.instanceOf(Vers);
+    });
+  });
+  describe('fromToLatest()', function() {
+    it('should allow from to be declared', function() {
+      var inst = new Vers();
+      inst.translate(2, 3, function(obj) { obj.version = 3; });
+      return inst.fromToLatest(2, {}).should.become({version: 3});
     });
   });
   describe('to()', function() {
@@ -44,6 +51,22 @@ describe('Vers', function() {
       var inst = new Vers(function(obj) { return obj.v; });
       inst.translate(1, 2, function(obj) { obj.v = 2; });
       return inst.to(2, {}).should.reject;
+    });
+  });
+  describe('toLatest()', function() {
+    it('should infer the latest integer version', function() {
+      var inst = new Vers();
+      inst.translate(1, 2, function(obj) { obj.version = 2; });
+      inst.translate(2, 3, function(obj) { obj.version = 3; });
+      return inst.toLatest({}).should.become({version: 3});
+    });
+  });
+  describe('translate()', function() {
+    it('should allow going backwards', function() {
+      var inst = new Vers();
+      inst.translate(1, 2, function(obj) { obj.version = 2; },
+        function(obj) { obj.version = 1; });
+      return inst.to(1, {version: 2}).should.become({version: 1});
     });
   });
 });
